c75a6de84fb8db2e0003efb1835f82a62cbf637b
Samandar-Komilov <komilov.samandar07@gmail.com>
2025-04-19 12:46:35 +0500

    add: Basic TCP server done

--- c-server/src/Server.c
+++ c-server/src/Server.c
@@ -26,16 +26,32 @@
 
 
 void launch(Server *self){
+    if (bind(self->socket, (struct sockaddr *)&self->address, sizeof(self->address)) < 0) {
+        perror("Failed to bind socket...");
+        exit(1);
+    }
+
+    if ((listen(self->socket, self->queue)) < 0){
+        perror("Failed to listen socket...");
+        exit(1);
+    }
+
+    printf("===== Waiting for connections on port %d =====\n", self->port);
+
     while (1){
         char buffer[MAX_BUFFER_SIZE];
-        printf("===== Waiting for a connection... =====");
         int address_length = sizeof(self->address);
         int new_socket = accept(self->socket, (struct sockaddr *)&self->address, (socklen_t *)&address_length);
         read(new_socket, buffer, MAX_BUFFER_SIZE);
         printf("%s\n", buffer);
 
-        char *hello = "HTTP/1.1 200 OK\nDate: Wed, 17 Apr 2024 10:30:45 GMT\nServer: Apache/2.4.41 (Ubuntu)\nContent-Type: application/json\nContent-Length: 102\nConnection: keep-alive\nCache-Control: max-age=3600";
-        write(new_socket, hello, strlen(hello));
+        char *response = "HTTP/1.1 200 OK\r\n"
+                         "Content-Type: text/plain\r\n"
+                         "Content-Length: 13\r\n"
+                         "\r\n"
+                         "Hello, World!";
+        
+        write(new_socket, response, strlen(response));
         close(new_socket);
     }
 }
@@ -43,7 +59,6 @@ void launch(Server *self){
 
 
 Server server_constructor(int domain, int service, int protocol, uint32_t interface, int port, int queue) {
-    printf(">>>");
     Server server;
     server.domain = domain;
     server.service = service;
@@ -58,32 +73,12 @@ Server server_constructor(int domain, int service, int protocol, uint32_t interf
 
     server.socket = socket(domain, service, protocol);
 
-    printf(">>> %d %d", server.socket, server.port);
-
     if (server.socket == 0){
         perror("Failed to connect socket...");
         exit(1);
     }
 
-    printf(">>>");
-
-    if (bind(server.socket, (struct sockaddr *)&server.address, sizeof(server.address)) < 0) {
-        perror("Failed to bind socket...");
-        exit(1);
-    }
-
-    printf(">>>");
-
-    if ((listen(server.socket, server.queue)) < 0){
-        perror("Failed to listen socket...");
-        exit(1);
-    }
-
-    printf(">>>");
-
     server.launch = launch;
-
-    printf(">>>");
 
     return server;
 }
--- c-server/src/main.c
+++ c-server/src/main.c
@@ -15,7 +15,6 @@
 
 int main(void){
     Server TCPServer = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 8080, 10);
-    printf("%d", TCPServer.port);
     TCPServer.launch(&TCPServer);
 
     return 0;
