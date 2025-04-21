87a39daf9771cb717b5bacc2bb8b067e1c54beda
Samandar-Komilov <komilov.samandar07@gmail.com>
2025-04-21 17:21:32 +0500

    refactor: dynamic array implementation

--- c-server/src/DataStructures/dynamic_array.c
+++ c-server/src/DataStructures/dynamic_array.c
@@ -3,138 +3,166 @@
 // 1lang1server/c-server/DataStructures
 //
 // =========
-// 
+//
 //
 // Created by Samandar Komil on 18/04/2025.
-// 
 //
-// 
+//
+//
 // dynamic_array.c
 
-
 #include <stdlib.h>
+#include <stdio.h>
 #include "dynamic_array.h"
 
-
 // Utility functions
-int resize_array(DynamicArray* self, int new_capacity){
+int resize_array(DynamicArray *self, int new_capacity)
+{
     int *temp_arr = realloc(self->array, new_capacity * sizeof(int));
     if (!temp_arr) return DA_ERROR_ALLOC;
-    self->array = temp_arr;
+    self->array    = temp_arr;
     self->capacity = new_capacity;
-    return 0;
+    return DA_SUCCESS;
 }
-
 
 // DynamicArray "object" methods (function pointers)
 
-int append(DynamicArray* self, int value){
-    if (self->len == self->capacity){
+int append(DynamicArray *self, int value)
+{
+    if (!self) return DA_ERROR_NULLPTR;
+
+    if (self->len == self->capacity)
+    {
         self->capacity *= 2;
-        if (resize_array(self, self->capacity) < 0){
+        if (resize_array(self, self->capacity) < 0)
+        {
             return DA_ERROR_ALLOC;
         }
     }
     self->array[self->len++] = value;
 
-    return;
+    return DA_SUCCESS;
 }
 
+int insert(DynamicArray *self, int index, int value)
+{
+    if (!self) return DA_ERROR_NULLPTR;
 
-int insert(DynamicArray* self, int index, int value){
-    /*
-    Validations:
-        - insert index out of range
-        - insert at the end
-        - insert at the beginning
-        - insert in the middle
-    */
-    if (index >= self->len || index < 0){
+    if (index > self->len || index < 0)
+    {
         return DA_ERROR_INDEX;
     }
 
     // Insert in the beginning
-    if (index == 0){
-        for (int i=self->len; i>0; i--){
-            self->array[i] = self->array[i-1];
+    if (index == 0)
+    {
+        for (int i = self->len; i > 0; i--)
+        {
+            self->array[i] = self->array[i - 1];
         }
         self->array[0] = value;
-        return;
+        return DA_SUCCESS;
     }
 
     // Insert in the end
-    else if (index == self->len){
-        if (self->len == self->capacity){
+    else if (index == self->len)
+    {
+        if (self->len == self->capacity)
+        {
             self->capacity *= 2;
-            if (resize_array(self, self->capacity) < 0){
+            if (resize_array(self, self->capacity) < 0)
+            {
                 return DA_ERROR_ALLOC;
             }
         }
         self->array[self->len++] = value;
-        return;
+        return DA_SUCCESS;
     }
 
     // Insert in the middle
-    else{
-        for (int i=self->len; i>index; i--){
-            if (self->len == self->capacity){
+    else
+    {
+        for (int i = self->len; i > index; i--)
+        {
+            if (self->len == self->capacity)
+            {
                 self->capacity *= 2;
-                int is_resized = resize_array(self, self->capacity);
-                if (is_resized < 0){
-                    perror("Failed to resize array: DynamicArray::insert()");
-                    exit(1);
+                if (resize_array(self, self->capacity) < 0)
+                {
+                    return DA_ERROR_ALLOC;
                 }
             }
-            self->array[i] == self->array[i-1];
+            self->array[i] = self->array[i - 1];
         }
         self->array[index] = value;
 
         self->len++;
-        return;
+        return DA_SUCCESS;
     }
 }
 
-
-int pop(DynamicArray* self){
-    int removed_element = self->array[--self->len];
+int pop(DynamicArray *self)
+{
+    if (!self) return DA_ERROR_NULLPTR;
+    int removed_element    = self->array[--self->len];
     self->array[self->len] = 0;
 
-    if (self->len == self->capacity * 0.25){
+    if (self->len == self->capacity * 0.25)
+    {
         self->capacity /= 2;
-        if (resize_array(self, self->capacity) < 0){
+        if (resize_array(self, self->capacity) < 0)
+        {
             return DA_ERROR_ALLOC;
         }
     }
-    
+
     return removed_element;
 }
 
-
-int remove(DynamicArray* self, int index){
-    // Very basic implementation
+int pop_at(DynamicArray *self, int index)
+{
+    if (!self) return DA_ERROR_NULLPTR;
     int removed_element = self->array[index];
-    for (int i=index; i<self->len; i++){
-        self->array[i] = self->array[i+1];
+    for (int i = index; i < self->len; i++)
+    {
+        self->array[i] = self->array[i + 1];
     }
     self->array[--self->len] = 0;
 
     return removed_element;
 }
 
+void list_elements(DynamicArray *self)
+{
+    for (int i = 0; i < self->len; i++)
+    {
+        printf("%d ", self->array[i]);
+    }
+    printf("\n");
+}
 
 // Constructor function
 
-DynamicArray dynamic_array_init(int capacity){
+DynamicArray dynamic_array_init(int capacity)
+{
     DynamicArray array;
-    array.array = malloc(capacity * sizeof(int));
-    array.len = 0;
-    array.capacity = capacity ? capacity >= 2 : 2;
+    array.array    = malloc(capacity * sizeof(int));
+    array.len      = 0;
+    array.capacity = (capacity >= 2) ? capacity : 2;
 
     // So-called "methods" of DynamicArray struct
-    array.append = append;
-    array.insert = insert;
-    array.pop = pop;
-    array.remove = remove;
+    array.append        = append;
+    array.insert        = insert;
+    array.pop           = pop;
+    array.pop_at        = pop_at;
+    array.list_elements = list_elements;
 
     return array;
+}
+
+int dynamic_array_free(DynamicArray *self)
+{
+    if (!self) return DA_ERROR_NULLPTR;
+    free(self->array);
+    return 0;
 }
--- c-server/src/DataStructures/dynamic_array.h
+++ c-server/src/DataStructures/dynamic_array.h
@@ -3,41 +3,42 @@
 // 1lang1server/c-server/DataStructures
 //
 // =========
-// 
+//
 //
 // Created by Samandar Komil on 18/04/2025.
-// 
 //
-// 
+//
+//
 // dynamic_array.c
 
-
-
-enum {
-    DA_SUCCESS = 0,
-    DA_ERROR_ALLOC = -1,
-    DA_ERROR_INDEX = -2
+enum
+{
+    DA_SUCCESS       = 0,
+    DA_ERROR_ALLOC   = -1,
+    DA_ERROR_INDEX   = -2,
+    DA_ERROR_NULLPTR = -3
 };
 
-
-
-typedef struct DynamicArray {
+typedef struct DynamicArray
+{
     int *array;
     int len;
     int capacity;
 
-    void (*append)(struct DynamicArray *self, int value);
-    void (*insert)(struct DynamicArray *self, int index, int value);
+    int (*append)(struct DynamicArray *self, int value);
+    int (*insert)(struct DynamicArray *self, int index, int value);
     int (*pop)(struct DynamicArray *self);
-    int (*remove)(struct DynamicArray *self, int index);
+    int (*pop_at)(struct DynamicArray *self, int index);
+    void (*list_elements)(struct DynamicArray *self);
 } DynamicArray;
-
 
 // Prototypes
 
 DynamicArray dynamic_array_init(int capacity);
+int dynamic_array_free(DynamicArray *self);
 
 int append(DynamicArray *self, int value);
 int insert(DynamicArray *self, int index, int value);
 int pop(DynamicArray *self);
-int remove(DynamicArray *self, int index);
+int pop_at(DynamicArray *self, int index);
+void list_elements(DynamicArray *self);
